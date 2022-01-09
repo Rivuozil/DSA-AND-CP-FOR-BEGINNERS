f8212223bd44d7419953b44ef6625eee4e65f89f
Rivuozil <84958582+Rivuozil@users.noreply.github.com>
2022-01-09 14:21:10 +0530

    Add files via upload

---
+++ B_Queue_at_the_School.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+int main(){
+int t,n;
+cin>>n>>t;
+
+string s;
+cin>>s;
+
+for(int j=0;j<t;j++)
+{for(int i=0;i<n-1;i++)
+{
+    if((s[i]=='B')&&(s[i+1]=='G'))
+    {swap(s[i],s[i+1]);
+     i++;}
+
+    
+}
+}
+
+cout<<s<<endl;
+return 0;
+}
